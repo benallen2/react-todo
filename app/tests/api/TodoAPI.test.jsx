a95706e972fe22d5c429d2271f72b7d4cb270379
Ben Allen <ben.allen2@gmail.com>
2017-01-18 15:52:29 -0500

    search functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -49,4 +49,37 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     })
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: "some text",
+      completed: true
+    },{
+      id: 2,
+      text: "some other text",
+      completed: false
+    },{
+      id: 3,
+      text: "some extra text",
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+    it('should return non completed if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    })
+  });
 });
