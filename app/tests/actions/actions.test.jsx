a64d904475b2e5a74c9193f5562cce2c71a822a8
Ben Allen <ben.allen2@gmail.com>
2017-02-08 14:23:44 -0500

    added actions and tests

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,44 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('Actions', () => {
+  it('Should generate search Text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Some Search Text'
+    };
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate add todo funciton', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Thing To do'
+    };
+    var res = actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('Should toggle Todos', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    }
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should toggle todo based on id', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 2
+    }
+
+    var res = actions.toggleTodo(action.id);
+
+    expect(res).toEqual(action);
+  });
+});
